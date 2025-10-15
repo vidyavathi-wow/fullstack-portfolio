36675aed7f3d8e724327b13f87c6160aedb6fc74
vidyavathi-wow <vidyavathi.telugu@wowlaz.com>
2025-10-15 07:45:45 +0530

    chore: checkhusky

--- frontend/todo-list-app/src/components/About/About.jsx
+++ frontend/todo-list-app/src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "./About.module.css"
-const About = () => {
+const About = ( => {
   return (
     <div className={styles.container}>
       <h1>This is About Component</h1>
